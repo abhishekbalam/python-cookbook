70497b54570f154a265ced4c4d1de2f7307e4c24
Abhishek Balam <abhishekbalam96@gmail.com>
2017-06-26 02:20:37 +0530

    Hello World

---
+++ hello_world/index.md
@@ -0,0 +1,5 @@
+# Hello World 
+---
+##### Prints hello word on the terminal, as per tradition.
+---
+
---
+++ hello_world/main.py
@@ -0,0 +1,10 @@
+""" Sample Python Scipt File"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    print("Hello World")
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
